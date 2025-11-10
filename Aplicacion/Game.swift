84c954a17c491dc14cdae9c53ac2b9088dd02711
Mariona Arenillas <mariona.arenillas@gracia.lasalle.cat>
2025-11-10 17:16:14 +0100

    Creacio Game

---
+++ Aplicacion/Game.swift
@@ -0,0 +1,21 @@
+//
+//  Game.swift
+//  Aplicacion
+//
+//  Created by alumne on 10/11/2025.
+//
+
+import Foundation
+
+struct Game {
+    static let lowNumber: Double = 1
+    static let highNumber: Double = 100
+    
+    private(set) var guessNumber = Int.random(in: Int(lowNumber)...Int(highNumber))
+    private(set) var points = 0
+    
+    mutating func calculatePoints(sliderValue: Double) {
+        // Calcula els punts com a màxim menys la diferència absoluta
+        self.points = Int(Game.highNumber) - abs(guessNumber - Int(sliderValue))
+    }
+}
